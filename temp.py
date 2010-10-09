30cc3e527aac03dcb06eff3ba4878116f0048242
Andrea Spadaccini <andrea.spadaccini@gmail.com>
2010-10-09 21:32:59 +0200

    Version 14: added an helper page for the 'schema' migration. Data will be inserted using the GAE administration tool

---
+++ temp.py
@@ -0,0 +1,19 @@
+from models import ApplicationData
+
+a = ApplicationData(name = 'twitter_oauth_consumer_key', value = '')
+a.put()
+
+a = ApplicationData(name = 'twitter_oauth_consumer_secret', value = '')
+a.put()
+
+a = ApplicationData(name = 'twitter_oauth_key', value = '')
+a.put()
+
+a = ApplicationData(name = 'twitter_oauth_secret', value = '')
+a.put()
+
+a = ApplicationData(name = 'answrcounter', value = '81')
+a.put()
+
+a = ApplicationData(name = 'lastansweredtweetid', value = '26865878036')
+a.put()
